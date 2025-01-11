1f51832e752692607b55b1595880a37f9ef74502
pupetes <javier.delapena.vico@gmail.com>
2025-01-11 20:41:20 +0100

    Added new particles

--- Main.java
+++ Main.java
@@ -15,14 +15,20 @@ public class Main {
     }
     // Main
     public static void main(String[] args){
-        // Define electron
-        Particle electron = new Particle("Electron",0.5f,-1f,0.5f);
-        System.out.println(electron.name+" quantum numbers: Mass "+electron.mass+" eV, Charge: "+ electron.charge +", Spin: "+ electron.spin);
-        // Define positron
-        Particle positron = new Particle("Positron",0.5f,1f,0.5f);
-        System.out.println(positron.name+" quantum numbers: Mass "+positron.mass+" eV, Charge: "+ positron.charge +", Spin: "+ positron.spin);
         // Define photon
         Particle photon = new Particle("Photon",0f,0f,1f);
-        System.out.println(photon.name+" quantum numbers: Mass "+photon.mass+" eV, Charge: "+ photon.charge +", Spin: "+ photon.spin);
+        System.out.println(photon.name+" quantum numbers: Mass "+photon.mass+" GeV, Charge: "+ photon.charge +", Spin: "+ photon.spin);
+        // Define electron
+        Particle electron = new Particle("Electron",0.5f,-1f,0.5f);
+        System.out.println(electron.name+" quantum numbers: Mass "+electron.mass+" GeV, Charge: "+ electron.charge +", Spin: "+ electron.spin);
+        // Define positron
+        Particle positron = new Particle("Positron",0.5f,1f,0.5f);
+        System.out.println(positron.name+" quantum numbers: Mass "+positron.mass+" GeV, Charge: "+ positron.charge +", Spin: "+ positron.spin);
+        // Define muon
+        Particle muon = new Particle("Muon",105.7f,-1f,0.5f);
+        System.out.println(muon.name+" quantum numbers: Mass "+muon.mass+" GeV, Charge: "+ muon.charge +", Spin: "+ muon.spin);
+        // Define antimuon
+        Particle antimuon = new Particle("Anti-muon",105.7f,1f,0.5f);
+        System.out.println(antimuon.name+" quantum numbers: Mass "+antimuon.mass+" GeV, Charge: "+ antimuon.charge +", Spin: "+ muon.spin);
     }
 }
--- master.py
+++ master.py
@@ -1,41 +1,6 @@
 # Libraries
 import sympy
-
-# Define particles
-class particle:
-
-    def __init__(self,name,mass,charge,spin,*args):
-        self.quantum_numbers=(charge,spin,*args) # Define the charge of the particles in natural units and the spin in absolute value.
-        self.mass=mass # MeV (Recomendation)
-        self.spin=spin # Do not enter natural numbers as float (example: spin 1 is valid, but not 1.0)
-        self.charge=charge
-        self.name=name
-        
-        if type(self.spin) == int:
-            self.type="boson"
-        elif type(self.spin) == float:
-            self.type="fermion"
-
-    def propagator(self,unicode1='\u03BC',unicode2='\u03BD'):
-
-        [k,m,eta,alpha,epsilon]=sympy.symbols('k,m,\u03B7,\u03B1,\u03B5', real=True)
-        mu=sympy.Symbol(unicode1, real=True)
-        nu=sympy.Symbol(unicode2, real=True)
-        gamma=sympy.Symbol('\u03B3', commutative=False)
-        Minkowiski_metric=eta**(mu*nu)
-
-        if (self.mass == 0) and (self.type == "boson"):
-            return (sympy.I*Minkowiski_metric)/(k**2+sympy.I*epsilon)
-        elif (self.mass != 0) and (self.type == "fermion"):
-            return (sympy.I*(k**(alpha)*gamma**(alpha)+m))/(k**2-m**2+sympy.I*epsilon)
-    
-    def external_legs(self):
-        [mu,nu]=[0,0]
-
-# Create particles
-photon = particle('photon',0,0,1) #In order to calculate amplitudes in QED, it is not necessary to include additional quantum numbers
-electron = particle('electron',0.5,-1,0.5)
-positron = particle('positron',0.5,1,0.5)
+from SM import photon, electron, positron, muon, antimuon, tau, antitau
 
 # Define QED vertex
 def QED_vertex(unicode):
@@ -47,7 +12,7 @@ def QED_vertex(unicode):
 
     return sympy.I*e*Q*gamma_mu
 
-# Define Feynman diagram
+# Define Feynman diagram (tree level)
 def QED_Feynman_diagram(propagated_particle):
     return QED_vertex('\u03BC')*propagated_particle.propagator()*QED_vertex('\u03BD')
 
@@ -55,4 +20,4 @@ def QED_Feynman_diagram(propagated_particle):
 print('Case in which a photon propagates')
 print(sympy.pretty(QED_Feynman_diagram(photon))) #Case in which a photon propagates
 print('Case in which a fermion propagates')
-print(sympy.pretty(QED_Feynman_diagram(electron))) #Case in which a fermion propagates. Note that QED_Feynman_diagram(positron) produces the same result.
+print(sympy.pretty(QED_Feynman_diagram(electron))) #Case in which a fermion propagates. Try with muons or taus...
